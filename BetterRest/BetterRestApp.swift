935725a87931a1d7d7f0cfc9b00ace9fce3753d2
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2021-05-04 20:54:09 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Robert Martinez on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
